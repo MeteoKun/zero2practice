75e7c47a3afe3a5eb4594479c8fbec8eca278876
BuildTools <unconfigured@null.spigotmc.org>
2020-06-19 13:29:46 -0700

    Static Method Practice

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car myCar = new Car();
+            myCar.Color = "Blue";
+
+            Car myCar2 = new Car();
+            myCar2.Color = "Red";
+
+            Console.WriteLine(myCar.Color);
+            Console.WriteLine(myCar2.Color);
+
+            //Math myMath = new Math();
+            int total = Math.Add(5, 3);
+
+            Console.WriteLine(total);
+
+            //Math myMath2 = new Math();
+            int total2 = Math.Add(6, 12);
+
+            Console.WriteLine(total2);
+
+
+
         }
     }
+
+    class Car
+    {
+        public string Color { get; set; }
+    }
+
+    class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
 }
+
+
+
