56e9a66ad679a7fd7d0ee7eed77f05c841bcd701
BuildTools <unconfigured@null.spigotmc.org>
2020-06-19 14:35:01 -0700

    Interface Tutorial

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,27 +6,54 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            IAnimal myAnimal;
 
-            Animal myAnimal = new Animal("Bob");
+            Console.WriteLine("What animal are you?");
+            string input = Console.ReadLine();
 
-            Console.WriteLine(myAnimal.Name);
-
+            if(input == "Dog")
+            {
+                myAnimal = new Dog();
+            }
+            else
+            {
+                myAnimal = new Animal();
+            }
+            //Animal myAnimal = new Animal();
+            myAnimal.Name = "Jessica";
+            //myAnimal.Color = "Purple";
+            myAnimal.Breathe();
+         
         }
     }
 
-    class Animal
+    class Animal : IAnimal
     {
-        public string Name { get; private set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
 
-        public Animal(string name)
+        public void Breathe()
         {
-            Name = name;
+            Console.WriteLine("I'm breathing");
         }
+    }
 
-        public void SetAnimalName(string name)
+    class Dog : IAnimal
+    {
+        public string Name { get; set; }
+
+        public void Breathe()
         {
-            Name = name;
+            Console.WriteLine("I'm a breathing dog");
         }
+    }
+
+    interface IAnimal
+    {
+        string Name { get; set; }
+
+        void Breathe();
+
     }
 
 }
